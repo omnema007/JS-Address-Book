b746d4ad8dba0b8519b025423fcec556ccc7dc78
Om Nema <134232774+omnema007@users.noreply.github.com>
2025-03-05 19:58:33 +0530

    [Om] Add. Implemented search functionality by city or state

--- addressbook.js
+++ addressbook.js
@@ -78,5 +78,9 @@ class AddressBook {
         }
         this.contacts.push(contact);
     }
+
+    searchByCityOrState(location) {
+        return this.contacts.filter(contact => contact.city === location || contact.state === location);
+    }
 }
 
